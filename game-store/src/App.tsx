1618fe0bddefb9c6ae7f4e97dac473bdcdf255e7
kundan <kundanbhagat842@gmail.com>
2024-01-06 18:07:08 +0530

    first commit

---
+++ game-store/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+}
+
+export default App;
