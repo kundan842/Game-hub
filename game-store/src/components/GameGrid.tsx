beab6ebeb24fcb02754efd43ea3172d69a49733d
kundan <kundanbhagat842@gmail.com>
2024-01-27 14:13:59 +0530

    dependency resloved

--- game-store/src/components/GameGrid.tsx
+++ game-store/src/components/GameGrid.tsx
@@ -16,29 +16,27 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skelatons = [1, 2, 3, 4, 5, 6, 7];
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-        marginTop={2}
-        // padding="10px"
-      >
-        {isLoading &&
-          skelatons.map((skelaton) => (
-            <GameCardContainer>
-              <LoadingSkelaton key={skelaton}></LoadingSkelaton>
-            </GameCardContainer>
-          ))}{" "}
-        {data.map((game) => (
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      marginTop={2}
+      // padding="10px"
+    >
+      {isLoading &&
+        skelatons.map((skelaton) => (
           <GameCardContainer>
-            {" "}
-            <GameCard key={game.id} game={game}></GameCard>
+            <LoadingSkelaton key={skelaton}></LoadingSkelaton>
           </GameCardContainer>
-        ))}
-      </SimpleGrid>
-    </>
+        ))}{" "}
+      {data.map((game) => (
+        <GameCardContainer>
+          {" "}
+          <GameCard key={game.id} game={game}></GameCard>
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
